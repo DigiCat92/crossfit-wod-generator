c859e65f815d1496a5c97e5b71c1901b8771a33b
DigiCat92 <147922230+DigiCat92@users.noreply.github.com>
2023-12-24 18:10:41 +0000

    Added basic js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 /** @format */
+function generateWod(event) {
+  event.preventDefault();
+
+  new Typewriter("#wod", {
+    strings: "La tombe dit à la rose",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let wodFormElement = document.querySelector("#wod-generator-form");
+wodFormElement.addEventListener("submit", generateWod);
